7cdbc561090e0b8b491bcbf20d2527ec01e7c5ed
Oleksyi <cutpaks@ukr.net>
2021-08-29 19:42:48 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exmo_trader_bot_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
