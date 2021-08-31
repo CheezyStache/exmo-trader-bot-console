2149018456f0a71036e29c54bac91c041a82b87b
Oleksyi <cutpaks@ukr.net>
2021-08-31 21:54:10 +0300

    Parser service init

---
+++ Models/Exmo/ExmoTrades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraderBot.Models.Exmo
+{
+    public class ExmoTrades
+    {
+        public long trade_id { get; set; }
+        public string type { get; set; }
+        public string price { get; set; }
+        public string quantity { get; set; }
+        public string amount { get; set; }
+        public long date { get; set; }
+    }
+}
---
+++ Services/ParserService/IParserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exmo_trader_bot_console.Services.ParserService
+{
+    public interface IParserService
+    {
+        T ParseResponse<T>(string response) where T : class;
+        IObservable<T> ParserStream<T>(IObservable<string> responseStream) where T : class;
+    }
+}
